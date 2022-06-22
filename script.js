ba95ba166eefd782f10706e4762984caf15fc835
Matthew Downey <matthewdowney@Matthews-MacBook-Air.local>
2022-06-22 15:48:22 -0500

    created hamburger drop down menu

---
+++ script.js
@@ -0,0 +1,8 @@
+const showDropDown = () => {
+    const navbar = document.getElementById("navbar")
+    if (navbar.style.display === "flex") {
+        navbar.style.display = "none"
+    } else {
+        navbar.style.display = "flex"
+    }
+}
