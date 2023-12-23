6bc0caaac925bdd82f6f4c2066c4dcbe607e2b79
Matthew Downey <matthewdowney@Matthews-Air.attlocal.net>
2023-12-22 18:45:20 -0600

    refactored skills section to change layout and add progress bars

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,71 +1,52 @@
 import React from "react";
-import { Heading, Container, Center, HStack, VStack, Text, Divider } from "@chakra-ui/react";
+import { Heading, Container, Center, Flex } from "@chakra-ui/react";
 import SkillTile from "./skillTile";
 
 const Skills = () => {
 
-    const languagesSkillList = ['HTML', 'CSS', 'JavaScript', 'Typescript', 'Python']
-    const frontEndSkillList = ['jQuery', 'React', 'Bootstrap', 'ChakraUI', 'VSCode']
-    const backEndSkillList = ['Express.js', 'Node.js', 'Django', 'FastAPI', 'Flask']
-    const databaseSkillList = ['SQL', 'NoSQL', 'PostgreSQL', 'MongoDB', 'Databricks']
-    const VCandDevOpsSkillList = ['Git', 'Github', 'Gitlab', 'AWS', 'PyCharm']
-
+    const skillList = [
+        {name: 'HTML', proficiency: 100},
+        {name: 'CSS', proficiency: 100},
+        {name: 'JavaScript', proficiency: 100},
+        {name: 'Typescript', proficiency: 100},
+        {name: 'Python', proficiency: 100},
+        {name: 'Java', proficiency: 30},
+        {name: 'jQuery', proficiency: 100},
+        {name: 'React', proficiency: 95},
+        {name: 'Bootstrap', proficiency: 100},
+        {name: 'ChakraUI', proficiency: 100},
+        {name: 'VSCode', proficiency: 100},
+        {name: 'Express.js', proficiency: 90},
+        {name: 'Node.js', proficiency: 90},
+        {name: 'Django', proficiency: 80},
+        {name: 'FastAPI', proficiency: 90},
+        {name: 'Flask', proficiency: 90},
+        {name: 'Spring Boot', proficiency: 30},
+        {name: 'SQL', proficiency: 90},
+        {name: 'NoSQL', proficiency: 85},
+        {name: 'PostgreSQL', proficiency: 90},
+        {name: 'MongoDB', proficiency: 85},
+        {name: 'Databricks', proficiency: 65},
+        {name: 'Git', proficiency: 100},
+        {name: 'Github', proficiency: 100},
+        {name: 'Gitlab', proficiency: 100},
+        {name: 'AWS', proficiency: 50},
+        {name: 'PyCharm', proficiency: 100},
+    ]
     
     return (
-        <Center maxWidth='100%' h='100vh' id='skills'>
+        <Center maxWidth='100%' h='100%' id='skills'>
             <Container>
                 <Heading as='h2' size='2xl' textAlign='center' pb='2rem'>
                     some things i know
                 </Heading>
-                <HStack justifyContent='center'>
-                    <VStack>
-                        {languagesSkillList.map((skill) => {
-                            return (
-                                <SkillTile skillName={skill} />
-                            )
-                        })}
-                    </VStack>
-                    <Center height='22rem'>
-                        <Divider orientation='vertical' />
-                    </Center>
-                    <VStack>
-                        {frontEndSkillList.map((skill) => {
-                            return (
-                                <SkillTile skillName={skill} />
-                            )
-                        })}
-                    </VStack>
-                    <Center height='22rem'>
-                        <Divider orientation='vertical' />
-                    </Center>
-                    <VStack>
-                        {backEndSkillList.map((skill) => {
-                            return (
-                                <SkillTile skillName={skill} />
-                            )
-                        })}
-                    </VStack>
-                    <Center height='22rem'>
-                        <Divider orientation='vertical' />
-                    </Center>
-                    <VStack>
-                        {databaseSkillList.map((skill) => {
-                            return (
-                                <SkillTile skillName={skill} />
-                            )
-                        })}
-                    </VStack>
-                    <Center height='22rem'>
-                        <Divider orientation='vertical' />
-                    </Center>
-                    <VStack>
-                        {VCandDevOpsSkillList.map((skill) => {
-                            return (
-                                <SkillTile skillName={skill} />
-                            )
-                        })}
-                    </VStack>
-                </HStack>
+                <Flex wrap='wrap' justifyContent='center'>
+                    {skillList.map((skill) => {
+                        return (
+                            <SkillTile skill={skill} />
+                        )
+                    })}
+                </Flex>
             </Container>
         </Center>
     )
