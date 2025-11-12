785e41141f377d812fa14be152a3e5ad605918c3
Matthew Downey <matthewdowney@Matthews-MacBook-Air.local>
2025-11-12 12:37:57 -0600

    fixed issue with skills section on mobile overflowing into other sections

--- src/components/skillTile.tsx
+++ src/components/skillTile.tsx
@@ -2,27 +2,27 @@ import React from "react";
 import { Text, Box } from "@chakra-ui/react";
 
 interface skillTileProps {
-    skill: string
+  skill: string;
 }
 
-const SkillTile = ({skill}: skillTileProps) => {
-    return (
-        <Box 
-            bg='rgba(0, 0, 0, 0.5)' 
-            p='1rem' 
-            m='0.25rem' 
-            borderRadius='1.5rem' 
-            display='flex' 
-            flexDirection='column' 
-            justifyContent='center' 
-            alignItems='center' 
-            data-aos='fade-up'
-        >
-            <Text textAlign='center' textColor='gray.300' data-aos='fade-up'>
-                {skill}
-            </Text>
-        </Box>
-    )
-}
+const SkillTile = ({ skill }: skillTileProps) => {
+  return (
+    <Box
+      bg="rgba(0, 0, 0, 0.5)"
+      p="0.5rem"
+      m="0.25rem"
+      borderRadius="1.5rem"
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      data-aos="fade-up"
+    >
+      <Text textAlign="center" textColor="gray.300" data-aos="fade-up">
+        {skill}
+      </Text>
+    </Box>
+  );
+};
 
-export default SkillTile
+export default SkillTile;
